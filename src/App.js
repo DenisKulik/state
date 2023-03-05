b8fdeb0a39e43cdd122c8409e2b596288f286bdd
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-05 16:35:27 +0700

    add a maximum value property to generate a random number

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import RandomNumber from './components/RandomNumber';
 
 function App() {
-    return (<div className="App"><RandomNumber/></div>);
+    return (<div className="App"><RandomNumber maxNum={100}/></div>);
 }
 
 export default App;
