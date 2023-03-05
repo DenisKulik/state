a8086038a8d3a9fda9eed3042408bf43064981c4
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-05 14:47:52 +0700

    add state for the RandomNumber component

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 function RandomNumber() {
+    const [ randomNum, setRandomNum ] = useState(100);
+
     return (<div>
-        <h1>100</h1>
+        <h1>{randomNum}</h1>
         <button>Generate new random number</button>
     </div>);
 }
